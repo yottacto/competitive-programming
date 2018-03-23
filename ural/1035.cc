d02d7350bcd5aa75857ddd60d2b35ff9e93ed03a
yottacto <yotta.w.cto@gmail.com>
2018-03-23 14:31:50 +0800

    [+] disjoint set and euler paths in directed graph

--- ural/1035.cc
+++ ural/1035.cc
@@ -1,40 +1,83 @@
 // ml:run = $bin < input
 #include <iostream>
 #include <algorithm>
+#include <tuple>
 
-auto constexpr maxn = 222;
-char map[2][maxn][maxn];
+int const maxn = 300 * 300;
+int parent[maxn];
+bool vis[maxn];
+std::tuple<int, int> ans[maxn];
+int g[2][4][maxn];
+char data[2][maxn];
 int n, m;
 
-// side 0 front, 1 back
-int count(int x, int y, int side)
+template <class T> T abs(T x) { return x < 0 ? -x : x; }
+
+int get_parent(int x)
 {
-    return (map[side][x][y] == '\\')
-        + (map[side][x][y + 1] == '/')
-        + (map[side][x + 1][y] == '/')
-        + (map[side][x + 1][y + 1] == '\\');
+    return x == parent[x]
+        ? x
+        : parent[x] = get_parent(parent[x]);
+}
+
+void set_union(int u, int v)
+{
+    parent[get_parent(u)] = get_parent(v);
+}
+
+enum dir { ul, ur, dr, dl };
+int count(int id, int side)
+{
+    return g[side][ul][id] + g[side][ur][id]
+        + g[side][dl][id] + g[side][dr][id];
 }
 
 int main()
 {
     std::cin >> n >> m;
-    for (auto f = 0; f < 2; f++)
+    auto tot = (n + 1) * (m + 1);
+
+    for (auto i = 0; i <= tot; i++)
+        parent[i] = i;
+
+    for (auto s = 0; s < 2; s++)
         for (auto i = 1; i <= n; i++)
-            for (auto j = 1; j <= m; j++)
-                std::cin >> map[f][i][j];
+            for (auto j = 1; j <= m; j++) {
+                auto id = (i - 1) * (m + 1) + (j - 1);
+                char ch;
+                std::cin >> ch;
+                data[s][id] = ch;
+                if (ch == 'X' || ch == '\\') {
+                    set_union(id, id + m + 2);
+                    g[s][dr][id] = 1;
+                    g[s][ul][id + m + 2] = 1;
+                }
+                if (ch == 'X' || ch == '/') {
+                    set_union(id + 1, id + m + 1);
+                    g[s][dl][id + 1] = 1;
+                    g[s][ur][id + m + 1] = 1;
+                }
+            }
 
-    auto front = 0;
-    auto back = 0;
-    for (auto i = 0; i <= n + 1; i++)
-        for (auto j = 0; j <= m + 1; j++) {
-            auto tmp = count(i, j, 0) - count(i, j, 1);
-            if (tmp > 0)
-                front += tmp;
-            else
-                back -= tmp;
-        }
+    for (auto i = 0; i <= tot; i++) {
+        auto pi = get_parent(i);
+        if (count(i, 0) || count(i, 1))
+            vis[pi] = true;
+        auto diff = count(i, 0) - count(i, 1);
+        if (diff > 0)
+            std::get<0>(ans[pi]) += diff;
+        else
+            std::get<1>(ans[pi]) -= diff;
+    }
 
-    std::cerr << front << " " << back << "\n";
-    std::cout << std::min(front, back) + abs(front - back) / 2 << "\n";
+    auto res = 0;
+    for (auto i = 0; i <= tot; i++) {
+        if (!vis[i]) continue;
+        auto front = std::get<0>(ans[i]);
+        auto back  = std::get<1>(ans[i]);
+        auto tmp = std::min(front, back) + abs(front - back) / 2;
+        res += tmp ? tmp : 1;
+    }
+    std::cout << res << "\n";
 }
 
