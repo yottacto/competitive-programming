7864e7347b40f88b99a384c40b7597fa447ba6c5
yottacto <yotta.w.cto@gmail.com>
2018-04-21 12:32:20 +0800

    [@] add a memory pool stil MLE...

--- ural/2096.cc
+++ ural/2096.cc
@@ -1,6 +1,9 @@
 // ml:run = $bin < input
 #include <iostream>
+#include <utility>
+#include <algorithm>
 #include <vector>
+#include <unordered_map>
 
 struct query { int day; int plan; int id; };
 struct change { int first; int tot; int period; int act; };
@@ -13,47 +16,90 @@ auto& operator>>(std::istream& is, change& c)
     return is;
 }
 
+auto constexpr maxn = 100'010;
+auto constexpr maxchange = 5'000'010;
+
+template <class T, int N, int MAXN>
+struct pool
+{
+    pool()
+    {
+        std::fill(next, next + MAXN, 0);
+        std::fill(head, head + N, 0);
+    }
+
+    void push(int id, T&& v)
+    {
+        value[alloc] = v;
+        if (!head[id]) {
+            tail[id] = alloc;
+            head[id] = alloc++;
+        } else {
+            next[tail[id]] = alloc;
+            tail[id] = alloc++;
+        }
+    }
+
+    T value[MAXN];
+    int next[MAXN];
+    int head[N];
+    int tail[N];
+    int alloc{1};
+};
+
 int n, m;
-std::vector<int> a;
-std::vector<std::vector<int>> g;
-std::vector<std::vector<query>> queries;
-std::vector<std::vector<change>> changes;
-std::vector<std::vector<restore>> restores;
-std::vector<std::vector<int>> ans;
+int a[maxn];
+std::vector<int> g[maxn];
+// std::vector<query> queries[maxn];
+pool<query, maxn, maxn> queries;
+// std::vector<change> changes[maxn];
+pool<change, maxn, maxn> changes;
+pool<restore, maxn, maxchange> restores;
+// std::unordered_map<int, int> restores[maxn];
+std::vector<int> ans[maxn];
 
 void add_edge(int u, int v)
 {
     g[u].emplace_back(v);
-    g[v].emplace_back(u);
 }
 
 void forward_change(int u)
 {
-    for (auto const& c : changes[u])
+    // for (auto const& c : changes[u])
+    for (auto p = changes.head[u]; p; p = changes.next[p]) {
+        auto const& c = changes.value[p];
         for (auto d = c.first, i = 0; i < c.tot; d += c.period, i++)
-            restores[u].emplace_back(restore{d, a[d]});
+            restores.push(u, {d, a[d]});
+                // restores[u][d] = a[d];
+            // restores[u].emplace_back(restore{d, a[d]});
+    }
 
-    for (auto const& c : changes[u])
+    // for (auto const& c : changes[u])
+    for (auto p = changes.head[u]; p; p = changes.next[p]) {
+        auto const& c = changes.value[p];
         for (auto d = c.first, i = 0; i < c.tot; d += c.period, i++)
             a[d] = c.act;
+    }
 }
 
 void backward_change(int u)
 {
-    for (auto const& r : restores[u])
+    for (auto p = restores.head[u]; p; p = restores.next[p]) {
+        auto const& r = restores.value[p];
         a[r.day] = r.act;
+    }
 }
 
-void dfs(int u, int parent)
+void dfs(int u)
 {
     forward_change(u);
-    for (auto const& q : queries[u])
+    // for (auto const& q : queries[u])
+    for (auto p = queries.head[u]; p; p = queries.next[p]) {
+        auto const& q = queries.value[p];
         ans[q.plan][q.id] = a[q.day];
-    for (auto v : g[u]) {
-        if (v == parent)
-            continue;
-        dfs(v, u);
     }
+    for (auto v : g[u])
+        dfs(v);
     backward_change(u);
 }
 
@@ -61,15 +107,18 @@ int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin >> n >> m;
-    a.resize(n);
-    g.resize(m + 1);
-    queries.resize(m + 1);
-    changes.resize(m + 1);
-    restores.resize(m + 1);
-    ans.resize(m);
 
-    for (auto& i : a)
-        std::cin >> i;
+    // auto size = (sizeof(changes) + sizeof(queries) + sizeof(restores));
+    // std::cout << size/1024/1024 << "\n";
+
+    // a.resize(n);
+    // g.resize(m + 1);
+    // queries.resize(m + 1);
+    // changes.resize(m + 1);
+    // restores.resize(m + 1);
+
+    for (auto i = 0; i < n; i++)
+        std::cin >> a[i];
 
     for (auto i = 0; i < m; i++) {
         int q;
@@ -78,21 +127,27 @@ int main()
         for (auto id = 0; id < q; id++) {
             int plan, day;
             std::cin >> plan >> day;
-            queries[plan].emplace_back(query{day - 1, i, id});
+            // queries[plan].emplace_back(query{day - 1, i, id});
+            queries.push(plan, query{day - 1, i, id});
         }
         int basis, count;
         std::cin >> basis >> count;
         add_edge(basis, i + 1);
-        changes[i + 1].resize(count);
-        for (auto& c : changes[i + 1])
+        // changes[i + 1].resize(count);
+        while (count--) {
+            change c;
             std::cin >> c;
+            changes.push(i + 1, std::move(c));
+        }
+        // for (auto& c : changes[i + 1])
+        //     std::cin >> c;
     }
 
-    dfs(0, -1);
+    dfs(0);
 
-    for (auto const& v : ans) {
-        for (auto i : v)
-            std::cout << i << " ";
+    for (auto i = 0; i < m; i++) {
+        for (auto j : ans[i])
+            std::cout << j << " ";
         std::cout << "\n";
     }
 }
