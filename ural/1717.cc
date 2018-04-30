12c2c12cc6f93c085201f7e5ca9d96087de490d1
yottacto <yotta.w.cto@gmail.com>
2018-04-30 23:33:08 +0800

    [@] segment tree, arithmetic progression

--- ural/1717.cc
+++ ural/1717.cc
@@ -17,11 +17,13 @@ struct node
 {
     bool none;
     ll sum;
+    // this max requests at least 2 poitnts
     ll max; ll lmax; ll rmax;
     int l;
     int r;
     int lmax_r;
     int rmax_l;
+    int count;
 };
 
 node tree[maxn * 6];
@@ -40,6 +42,7 @@ std::vector<data> a;
 void build(int id, int l, int r)
 {
     tree[id].none = true;
+
     if (l == r) return;
     auto mid = (l + r) / 2;
     build(id * 2, l, mid);
@@ -62,13 +65,15 @@ void push_up(int id)
             t.max    = tl.max;
             t.lmax   = tl.lmax;
             t.lmax_r = tl.lmax_r;
-            t.rmax   = -inf;
+            t.rmax   = tl.rmax;
+            t.rmax_l = tl.rmax_l;
             t.l      = tl.l;
             t.r      = tl.r;
         } else {
             t.sum    = tr.sum;
             t.max    = tr.max;
-            t.lmax   = -inf;
+            t.lmax   = tr.lmax;
+            t.lmax_r = tr.lmax_r;
             t.rmax   = tr.rmax;
             t.rmax_l = tr.rmax_l;
             t.l      = tr.l;
@@ -81,19 +86,17 @@ void push_up(int id)
     t.sum = tl.sum + tr.sum;
     t.lmax = tl.lmax;
     t.lmax_r = tl.lmax_r;
-    if (tl.lmax == tl.sum)
-        if (tl.sum + tr.lmax > t.lmax) {
-            t.lmax = tl.sum + tr.lmax;
-            t.lmax_r = tr.lmax_r;
-        }
+    if (tl.sum + tr.lmax > t.lmax) {
+        t.lmax = tl.sum + tr.lmax;
+        t.lmax_r = tr.lmax_r;
+    }
 
     t.rmax = tr.rmax;
     t.rmax_l = tr.rmax_l;
-    if (tr.rmax == tr.sum)
-        if (tr.sum + tl.rmax > t.rmax) {
-            t.rmax = tr.sum + tl.rmax;
-            t.rmax_l = tl.rmax_l;
-        }
+    if (tr.sum + tl.rmax > t.rmax) {
+        t.rmax = tr.sum + tl.rmax;
+        t.rmax_l = tl.rmax_l;
+    }
 
     t.max = tl.max;
     t.l = tl.l;
@@ -118,10 +121,15 @@ void update(int id, int l, int r, int p, ll delta)
         auto& t = tree[id];
         if (t.none) {
             t.none = false;
-            t.sum = 0;
+            t.sum = t.count = 0;
         }
+        t.count++;
         t.sum += delta;
-        t.max = t.lmax = t.rmax = t.sum;
+        if (t.count == 1)
+            t.max = -inf;
+        else
+            t.max = t.sum;
+        t.lmax = t.rmax = t.sum;
         t.l = t.r = t.lmax_r = t.rmax_l = p;
         return;
     }
@@ -157,20 +165,17 @@ int main()
             || (lhs.x == rhs.x && lhs.y < rhs.y);
     });
 
-    auto single_max = -inf;
-    std::for_each(std::begin(a), std::end(a), [&](data const& x) {
-        single_max = std::max(single_max, x.value);
-    });
-
     auto ans = -inf;
     int xl, xr;
     int yl, yr;
 
     for (auto i = 0; i < n; i++) {
+        if (i && a[i].x == a[i - 1].x)
+            continue;
         build(1, 1, tot);
         for (auto j = i; j < n; j++) {
             update(1, 1, tot, a[j].y, a[j].value);
-            for (j++; j < n && a[j].y == a[j - 1].y; j++)
+            for (j++; j < n && a[j].x == a[j - 1].x; j++)
                 update(1, 1, tot, a[j].y, a[j].value);
             j--;
 
@@ -181,49 +186,14 @@ int main()
                 yl = disc[tree[1].l - 1];
                 yr = disc[tree[1].r - 1];
             }
-
-            #if DEBUG
-            if (i == 2) {
-                std::cerr << j << " " << tree[1].max << "\n";
-            }
-            #endif
-        }
-    }
-
-    if (ans == single_max) {
-        ans = -inf;
-        for (auto i = 0; i < n; i++) {
-            auto ymax = -inf;
-            auto ymin = +inf;
-            for (auto j = i + 1; j < n; j++) {
-                if (a[j].y >= a[i].y) {
-                    if (ymin > a[j].y && a[j].value + a[i].value > ans) {
-                        ans = a[j].value + a[i].value;
-                        xl = a[i].x;
-                        xr = a[j].x;
-                        yl = disc[a[i].y - 1];
-                        yr = disc[a[j].y - 1];
-                    }
-                    ymin = std::min(ymin, a[j].y);
-                }
-                if (a[j].y <= a[i].y) {
-                    if (ymax < a[j].y && a[j].value + a[i].value > ans) {
-                        ans = a[j].value + a[i].value;
-                        xl = a[i].x;
-                        xr = a[j].x;
-                        yl = disc[a[j].y - 1];
-                        yr = disc[a[i].y - 1];
-                    }
-                    ymax = std::max(ymax, a[j].y);
-                }
-            }
         }
     }
 
     #if DEBUG
-    for (auto const& i : a)
-        std::cerr << i.y << " ";
-    std::cerr << "\n";
+    // for (auto const& i : a)
+    //     std::cerr << i.y << " ";
+    // std::cerr << "\n";
+
     std::cerr << ans << "\n";
     #endif
 
