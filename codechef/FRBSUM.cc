02934bf4e0f81228f5a8de144927b5eadfce86ef
yottacto <yotta.w.cto@gmail.com>
2018-11-05 11:25:14 +0800

    Add FRBSUM, TLE

---
+++ codechef/FRBSUM.cc
@@ -0,0 +1,75 @@
+// ml:run = $bin < input
+// ml:opt = 0
+// ml:ccf += -g
+#include <iostream>
+#include <utility>
+
+auto constexpr maxn = 100007;
+auto constexpr maxr = 1000000000;
+int root [maxn];
+int n, m;
+
+struct node
+{
+    int l;
+    int r;
+    int count;
+};
+
+node tree[40 * maxn];
+int alloc;
+
+void update(int& now, int pre, int pos, int l = 1, int r = maxr)
+{
+    tree[now = ++alloc] = tree[pre];
+    tree[now].count++;
+
+    if (l == r) return;
+    auto mid = (l + r) / 2;
+    if (pos <= mid)
+        update(tree[now].l, tree[pre].l, pos, l, mid);
+    else
+        update(tree[now].r, tree[pre].r, pos, mid + 1, r);
+}
+
+auto get_kth(int now, int pre, int k, int l = 1, int r = maxr) -> std::pair<int, int>
+{
+    if (l == r)
+        return {l, tree[now].count - tree[pre].count};
+    auto mid = (l + r) / 2;
+    auto left = tree[tree[now].l].count - tree[tree[pre].l].count;
+    if (k <= left)
+        return get_kth(tree[now].l, tree[pre].l, k, l, mid);
+    else
+        return get_kth(tree[now].r, tree[pre].r, k - left, mid + 1, r);
+}
+
+auto forbidden_sum(int l, int r) -> int
+{
+    auto sum = 0;
+    for (auto i = 1; i <= r - l + 1; ) {
+        auto kth = get_kth(root[r], root[l - 1], i);
+        if (sum + 1 < kth.first)
+            return sum + 1;
+        sum += kth.first * kth.second;
+        i += kth.second;
+    }
+    return sum + 1;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin >> n;
+    for (int i = 1, a; i <= n; i++) {
+        std::cin >> a;
+        update(root[i], root[i-1], a);
+    }
+
+    std::cin >> m;
+    for (int l, r; m--; ) {
+        std::cin >> l >> r;
+        std::cout << forbidden_sum(l, r) << "\n";
+    }
+}
+
