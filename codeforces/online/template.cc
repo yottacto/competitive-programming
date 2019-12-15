88acbc5ab0d56fab393bfe77693fde8eec895695
yottacto <yotta.w.cto@gmail.com>
2019-12-15 17:03:57 +0800

    Update cf template

--- codeforces/online/template.cc
+++ codeforces/online/template.cc
@@ -1,6 +1,34 @@
 // ml:run = $bin < input
 #include <bits/stdc++.h>
 
+using ll = long long;
+auto constexpr mo = 998244353ll;
+
+ll add(ll x, ll y)
+{
+    auto res = (x + y) % mo;
+    if (res < 0)
+        res += mo;
+    return res;
+}
+
+ll sub(ll x, ll y)
+{
+    auto res = (x - y) % mo;
+    if (res < 0)
+        res += mo;
+    return res;
+}
+
+ll mul(ll x, ll y)
+{
+    auto res = (x * y) % mo;
+    if (res < 0)
+        res += mo;
+    return res;
+}
+
+
 int main()
 {
     std::ios::sync_with_stdio(false);
